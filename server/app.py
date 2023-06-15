9732630fcd01393cad68278761a036c43538ba79
Historia Montague <historia.montague@outlook.com>
2023-06-15 12:01:58 -0600

    Finished Lab

--- server/app.py
+++ server/app.py
@@ -44,9 +44,27 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
-api.add_resource(PlantByID, '/plants/<int:id>')
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
         
+        for attr in request.get_json():
+            setattr(plant, attr, request.get_json().get(attr))
+        
+        db.session.add(plant)
+        db.session.commit()
+        
+        return make_response(plant.to_dict(), 200)
+            
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        db.session.delete(plant)
+        db.session.commit()
+        
+        return make_response("", 204)
+    
+api.add_resource(PlantByID, '/plants/<int:id>')        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
